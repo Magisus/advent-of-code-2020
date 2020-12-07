62a5d5173a4eff075de72f74cc14c22601d54c8f
Maggie Dreyer <darthmagisus@gmail.com>
2020-12-06 17:49:47 -0800

    Day 2 part 2

--- day2/password-policy.go
+++ day2/password-policy.go
@@ -20,30 +20,40 @@ func main() {
 
     entries := strings.Split(strings.Trim(string(data), "\n"), "\n")
 
-    minMaxRegex := regexp.MustCompile("^([0-9]+)-([0-9]+)")
+    positionsRegex := regexp.MustCompile("^([0-9]+)-([0-9]+)")
     requiredLetterRegex := regexp.MustCompile("([a-z]):")
     passwordRegex := regexp.MustCompile("[a-z]+$")
 
     validCount := 0
 
     for _, entry := range entries {
-        minMax := minMaxRegex.FindStringSubmatch(entry)
-        min, _ := strconv.Atoi(minMax[1])
-
-        max, _ := strconv.Atoi(minMax[2])
+        positions := positionsRegex.FindStringSubmatch(entry)
+        fmt.Println(positions[1] + " or " + positions[2])
+        // Policy is 1-indexed
+        pos1, _ := strconv.Atoi(positions[1])
+        pos2, _ := strconv.Atoi(positions[2])
+        pos1 = pos1 - 1
+        pos2 = pos2 - 1
 
         requiredLetter := requiredLetterRegex.FindStringSubmatch(entry)[1]
+        fmt.Println(requiredLetter)
 
         password := passwordRegex.FindString(entry)
+        fmt.Println(password)
 
-        count := 0
-        for _, c := range password {
-            if string(c) == requiredLetter {
-               count++
-            }
+        oneValidLetter := false
+        if pos1 < len(password) && string(password[pos1]) == requiredLetter {
+            oneValidLetter = !oneValidLetter
         }
-        if count >= min && count <= max {
+        if pos2 < len(password) && string(password[pos2]) == requiredLetter {
+            oneValidLetter = !oneValidLetter
+        }
+
+        if oneValidLetter {
+            fmt.Println(true)
             validCount++
+        } else {
+            fmt.Println(false)
         }
     }
     fmt.Println(validCount)
