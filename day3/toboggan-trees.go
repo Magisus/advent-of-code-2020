221a58141cdfe92212043bd11803a851a2597020
Maggie Dreyer <darthmagisus@gmail.com>
2020-12-07 23:57:16 -0800

    Day 3 part 2

--- day3/toboggan-trees.go
+++ day3/toboggan-trees.go
@@ -18,11 +18,7 @@ func IsTree(pos [2]int, rows []string) bool {
     return string(sym) == "#"
 }
 
-func main() {
-    data, err := ioutil.ReadFile("input.txt")
-    check(err)
-
-    rows := strings.Split(strings.Trim(string(data), "\n"), "\n")
+func CountTrees(slope [2]int, rows []string) int {
     width := len(rows[0])
     height := len(rows)
 
@@ -32,9 +28,24 @@ func main() {
         if IsTree(pos, rows) {
             treeCount++
         }
-        i++
+        i += slope[0]
         pos[0] = i
-        pos[1] = (pos[1] + 3) % width
+        pos[1] = (pos[1] + slope[1]) % width
     }
     fmt.Println("Tree count: " + strconv.Itoa(treeCount))
+    return treeCount
+}
+
+func main() {
+    data, err := ioutil.ReadFile("input.txt")
+    check(err)
+
+    rows := strings.Split(strings.Trim(string(data), "\n"), "\n")
+    treeCount1 := CountTrees([2]int{1, 1}, rows)
+    treeCount2 := CountTrees([2]int{1, 3}, rows)
+    treeCount3 := CountTrees([2]int{1, 5}, rows)
+    treeCount4 := CountTrees([2]int{1, 7}, rows)
+    treeCount5 := CountTrees([2]int{2, 1}, rows)
+    total := treeCount1 * treeCount2 * treeCount3 * treeCount4 * treeCount5
+    fmt.Println(total)
 }
